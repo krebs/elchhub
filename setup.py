b93f01e40fd25d44f4ad2bc56f15e1128a192427
makefu <github@syntax-fehler.de>
2016-11-01 11:57:26 +0100

    initial vomit (copy-paste from elchos)

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""
+elchHub Web interface
+"""
+from setuptools import setup
+
+setup(
+    name='elchHub',
+    version='1.0',
+    long_description=__doc__,
+    packages=['elchhub'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask',
+        'ftplib',
+        'requests>=2'
+    ],
+    url="http://github.com/krebscode/elchhub"
+)
+
