4ffbbc2b5ef1d6a0117e4a80fdc1e5c5122349b4
makefu <github@syntax-fehler.de>
2016-11-01 12:21:59 +0100

    fix wsgi

--- elchhub/wsgi.py
+++ elchhub/wsgi.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from elchhub.elchos import app
+from .elchos import app as application
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
