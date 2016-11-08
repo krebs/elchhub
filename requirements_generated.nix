e43048bc0577f2a88f8c433ecb76904aa4016179
makefu <github@syntax-fehler.de>
2016-11-08 11:10:50 +0100

    add nix build env

---
+++ requirements_generated.nix
@@ -0,0 +1,196 @@
+# generated using pypi2nix tool (version: 1.4.0.dev0)
+#
+# COMMAND:
+#   pypi2nix -V 3 -r Requirements.txt
+#
+
+{ pkgs, python, commonBuildInputs ? [], commonDoCheck ? false }:
+
+self: {
+
+  "Flask" = python.mkDerivation {
+    name = "Flask-0.11.1";
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/55/8a/78e165d30f0c8bb5d57c429a30ee5749825ed461ad6c959688872643ffb3/Flask-0.11.1.tar.gz";
+      sha256= "b4713f2bfb9ebc2966b8a49903ae0d3984781d5c878591cf2f7b484d28756b0e";
+    };
+    doCheck = commonDoCheck;
+    buildInputs = commonBuildInputs;
+    propagatedBuildInputs = [
+      self."Jinja2"
+      self."Werkzeug"
+      self."click"
+      self."itsdangerous"
+    ];
+    meta = with pkgs.stdenv.lib; {
+      homepage = "";
+      license = licenses.bsdOriginal;
+      description = "A microframework based on Werkzeug, Jinja2 and good intentions";
+    };
+  };
+
+
+
+  "Jinja2" = python.mkDerivation {
+    name = "Jinja2-2.8";
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/f2/2f/0b98b06a345a761bec91a079ccae392d282690c2d8272e708f4d10829e22/Jinja2-2.8.tar.gz";
+      sha256= "bc1ff2ff88dbfacefde4ddde471d1417d3b304e8df103a7a9437d47269201bf4";
+    };
+    doCheck = commonDoCheck;
+    buildInputs = commonBuildInputs;
+    propagatedBuildInputs = [
+      self."MarkupSafe"
+    ];
+    meta = with pkgs.stdenv.lib; {
+      homepage = "";
+      license = licenses.bsdOriginal;
+      description = "A small but fast and easy to use stand-alone template engine written in pure python.";
+    };
+  };
+
+
+
+  "MarkupSafe" = python.mkDerivation {
+    name = "MarkupSafe-0.23";
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/c0/41/bae1254e0396c0cc8cf1751cb7d9afc90a602353695af5952530482c963f/MarkupSafe-0.23.tar.gz";
+      sha256= "a4ec1aff59b95a14b45eb2e23761a0179e98319da5a7eb76b56ea8cdc7b871c3";
+    };
+    doCheck = commonDoCheck;
+    buildInputs = commonBuildInputs;
+    propagatedBuildInputs = [ ];
+    meta = with pkgs.stdenv.lib; {
+      homepage = "";
+      license = licenses.bsdOriginal;
+      description = "Implements a XML/HTML/XHTML Markup safe string for Python";
+    };
+  };
+
+
+
+  "Werkzeug" = python.mkDerivation {
+    name = "Werkzeug-0.11.11";
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/43/2e/d822b4a4216804519ace92e0368dcfc4b0b2887462d852fdd476b253ecc9/Werkzeug-0.11.11.tar.gz";
+      sha256= "e72c46bc14405cba7a26bd2ce28df734471bc9016bc8b4cb69466c2c14c2f7e5";
+    };
+    doCheck = commonDoCheck;
+    buildInputs = commonBuildInputs;
+    propagatedBuildInputs = [ ];
+    meta = with pkgs.stdenv.lib; {
+      homepage = "";
+      license = licenses.bsdOriginal;
+      description = "The Swiss Army knife of Python web development";
+    };
+  };
+
+
+
+  "click" = python.mkDerivation {
+    name = "click-6.6";
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/7a/00/c14926d8232b36b08218067bcd5853caefb4737cda3f0a47437151344792/click-6.6.tar.gz";
+      sha256= "cc6a19da8ebff6e7074f731447ef7e112bd23adf3de5c597cf9989f2fd8defe9";
+    };
+    doCheck = commonDoCheck;
+    buildInputs = commonBuildInputs;
+    propagatedBuildInputs = [ ];
+    meta = with pkgs.stdenv.lib; {
+      homepage = "";
+      license = licenses.bsdOriginal;
+      description = "A simple wrapper around optparse for powerful command line utilities.";
+    };
+  };
+
+
+
+  "ftputil" = python.mkDerivation {
+    name = "ftputil-3.3.1";
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/95/c1/216cb32e04168adbafe7439b6a186a11b74965af9efc93c89a4abdc5fac4/ftputil-3.3.1.tar.gz";
+      sha256= "bc88f35cc7f5f292ec4b56e99c8b05d361de1cc8b330050e32b0c4ecaa2d2b01";
+    };
+    doCheck = commonDoCheck;
+    buildInputs = commonBuildInputs;
+    propagatedBuildInputs = [ ];
+    meta = with pkgs.stdenv.lib; {
+      homepage = "";
+      license = licenses.bsdOriginal;
+      description = "High-level FTP client library (virtual file system and more)";
+    };
+  };
+
+
+
+  "itsdangerous" = python.mkDerivation {
+    name = "itsdangerous-0.24";
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/dc/b4/a60bcdba945c00f6d608d8975131ab3f25b22f2bcfe1dab221165194b2d4/itsdangerous-0.24.tar.gz";
+      sha256= "cbb3fcf8d3e33df861709ecaf89d9e6629cff0a217bc2848f1b41cd30d360519";
+    };
+    doCheck = commonDoCheck;
+    buildInputs = commonBuildInputs;
+    propagatedBuildInputs = [ ];
+    meta = with pkgs.stdenv.lib; {
+      homepage = "";
+      license = licenses.bsdOriginal;
+      description = "Various helpers to pass trusted data to untrusted environments and back.";
+    };
+  };
+
+
+
+  "minibar" = python.mkDerivation {
+    name = "minibar-0.5.0";
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/12/34/f1d8396d246f9649e164208d9c28e3c2bdb91077c59f86368b10fba36324/minibar-0.5.0.tar.gz";
+      sha256= "da7d1f8ccf4e7fe3b9952ab9dc6f9cee308f52a8c0903747a606c4d8b0353a2c";
+    };
+    doCheck = commonDoCheck;
+    buildInputs = commonBuildInputs;
+    propagatedBuildInputs = [ ];
+    meta = with pkgs.stdenv.lib; {
+      homepage = "";
+      license = licenses.mit;
+      description = "Simple text progress bar library";
+    };
+  };
+
+
+
+  "redis" = python.mkDerivation {
+    name = "redis-2.10.5";
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/68/44/5efe9e98ad83ef5b742ce62a15bea609ed5a0d1caf35b79257ddb324031a/redis-2.10.5.tar.gz";
+      sha256= "5dfbae6acfc54edf0a7a415b99e0b21c0a3c27a7f787b292eea727b1facc5533";
+    };
+    doCheck = commonDoCheck;
+    buildInputs = commonBuildInputs;
+    propagatedBuildInputs = [ ];
+    meta = with pkgs.stdenv.lib; {
+      homepage = "";
+      license = licenses.mit;
+      description = "Python client for Redis key-value store";
+    };
+  };
+
+
+
+  "requests" = python.mkDerivation {
+    name = "requests-2.11.1";
+    src = pkgs.fetchurl {
+      url = "https://pypi.python.org/packages/2e/ad/e627446492cc374c284e82381215dcd9a0a87c4f6e90e9789afefe6da0ad/requests-2.11.1.tar.gz";
+      sha256= "5acf980358283faba0b897c73959cecf8b841205bb4b2ad3ef545f46eae1a133";
+    };
+    doCheck = commonDoCheck;
+    buildInputs = commonBuildInputs;
+    propagatedBuildInputs = [ ];
+    meta = with pkgs.stdenv.lib; {
+      homepage = "";
+      license = licenses.asl20;
+      description = "Python HTTP for Humans.";
+    };
+  };
+
+}
